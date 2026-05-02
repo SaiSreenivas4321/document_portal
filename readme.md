740ae9d702f93ab33c45a7bdcac4418ee6ccb384
SaiSreenivas_nehask_Datascitist <sreenivassaibks@gmail.com>
2026-05-02 12:29:53 +0530

    created the entire files for documentPortal and set all files for devlopment and cretaed the entire files for project

---
+++ readme.md
@@ -0,0 +1,30 @@
+### commands need to follow for this project 
+
+
+"""
+Mkdir "Project Folders"
+"""
+
+
+"""
+cd project_folder
+"""
+
+
+"" conda setup and create virtual actvate 
+
+"""
+conda create -p venv python=version -y
+
+"""
+
+"""
+conda activate <virtual-env>
+"""
+
+## git commands
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="documentPortal_LLMOPS",
+    author="sai Sreenivas LLMops",
+    version="0.1",
+    description="A package for document portal LLM operations",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
